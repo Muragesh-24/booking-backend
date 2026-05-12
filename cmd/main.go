b6ae53e4417f77879a5194d2b911d73d94269131
Muragesh-24 <nyamagoudmurgesh@gmail.com>
2026-05-12 11:45:59 +0530

    delete : local env code

--- cmd/main.go
+++ cmd/main.go
@@ -10,15 +10,15 @@ import (
 
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
-	"github.com/joho/godotenv"
+	// "github.com/joho/godotenv"
 )
 
 func main() {
 
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatal(err)
-	}
+	// err := godotenv.Load()
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
 	db := scripts.ConnectDatabase()
 	port := os.Getenv("PORT")
 	if port == "" {
